37b0450ef9a294ee7c25e4e1d6c8919dc90c4894
MoisesLoar18 <moises.loartec@gmail.com>
2024-08-25 22:42:08 -0500

    prueba de commit

--- src/plugins/http-client.plugin.js
+++ src/plugins/http-client.plugin.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const httpClientPlugin = {
   get: async (url) => {
     const response = await axios.get(url);
-    console.log(response.data)
     return response.data;
   },
 };
